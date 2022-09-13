928f44d431df832047a79d9b9f37d04945a4b048
jonasvm0 <113418032+jonasvm0@users.noreply.github.com>
2022-09-13 08:49:25 +0200

    Add files via upload

---
+++ sketch_oct28c.ino
@@ -0,0 +1,21 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(2,OUTPUT);
+pinMode(3,OUTPUT);
+pinMode(4,OUTPUT);
+pinMode(5,OUTPUT);
+pinMode(6,OUTPUT);
+pinMode(7,OUTPUT);
+pinMode(8,OUTPUT);
+pinMode(9,OUTPUT);
+pinMode(10,OUTPUT);
+pinMode(11,OUTPUT);
+pinMode(12,OUTPUT);
+pinMode(13,OUTPUT);
+pinMode(1,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
